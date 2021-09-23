17ce5c8b908403598c71bbbeaff6943e827ce4be
OlivierArgentieri <olivierargentieri@gmail.com>
2021-09-23 17:29:23 +0200

    fix for linux

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const mainApp = require("../src/index")
 
 // eslint-disable-next-line no-undef
-describe("silex_socket_service INIT Server", () => {
+describe("silex_socket_service_init_server", () => {
   // eslint-disable-next-line no-undef
   before((done) => {
     mainApp.run()
