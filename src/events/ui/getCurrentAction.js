c226ab49ed1ed96dd811764dea706a959cc4b06c
OlivierArgentieri <olivierargentieri@gmail.com>
2021-10-07 15:54:04 +0200

    final fix caseSensitiveFile

---
+++ src/events/ui/getCurrentAction.js
@@ -0,0 +1,19 @@
+const store = require("../../store")
+const logger = require("../../plugins/logger")
+
+const getCurrentAction = (socket) => {
+  socket.on("getCurrentAction", (callback) => {
+    logger.info(` => [RECEIVED on /ui getCurrentAction] socketID: ${socket.data.uuid}`)
+    if (!callback) {
+      return
+    }
+    // eslint-disable-next-line node/no-callback-literal
+    callback({
+      status: 200,
+      msg: "ok",
+      data: store.instance.data.currentAction
+    })
+  })
+}
+
+module.exports = getCurrentAction
