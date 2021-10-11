7c60e3d578d5c4ae947f92cb7f0002a511678811
Joseph Henry <josephenry@protonmail.com>
2021-10-11 21:39:33 +0200

    build: add .env file config

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
+// Read the .env file
+require("dotenv-safe").config()
+
 const initListeners = require("./listeners")
 const app = require("express")()
 const http = require("http").createServer(app)
 const logger = require("./plugins/logger")
 
 const io = require("socket.io")(http, {
-  cors: { origins: ["http://localhost:3000"] }
+  cors: { origins: [process.env.SILEX_FRONT_URL] }
 })
 
 const run = async () => {
   // calls listeners
   initListeners(io)
 
-  http.listen(5118, function () {
-    logger.info("listening on *:5118")
+  http.listen(process.env.PORT, function () {
+    logger.info(`listening on *:${process.env.PORT}`)
   })
 }
 
