0f7d7861b9d0ee59a4673f586c55dfa7526204b0
OlivierArgentieri <olivierargentieri@gmail.com>
2021-10-21 11:32:14 +0200

    write log to file pino

--- src/plugins/logger.js
+++ src/plugins/logger.js
@@ -1,1 +1,6 @@
-module.exports = require("pino")({ prettyPrint: true })
+const pino = require("pino")
+const logger= pino(
+    { prettyPrint: true },
+    pino.destination("./silex_socket_service.log")
+)
+module.exports = logger
