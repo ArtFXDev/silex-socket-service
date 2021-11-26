73b75b0c2c88c62e22a4a88d1ce55a4c33051676
Joseph HENRY <josephenry@protonmail.com>
2021-11-26 19:53:19 +0100

    feat: diff utils

---
+++ src/utils/diff.js
@@ -0,0 +1,50 @@
+/**
+ * Taken from https://stackoverflow.com/questions/25456013/javascript-deepequal-comparison/25456134
+ */
+function deepEqual(x, y) {
+  if (x === y) {
+    return true;
+  } else if (
+    typeof x == "object" &&
+    x != null &&
+    typeof y == "object" &&
+    y != null
+  ) {
+    if (Object.keys(x).length != Object.keys(y).length) return false;
+
+    for (const prop in x) {
+      if (Object.prototype.hasOwnProperty.call(y, prop)) {
+        if (!deepEqual(x[prop], y[prop])) return false;
+      } else return false;
+    }
+
+    return true;
+  } else return false;
+}
+
+function isObject(o) {
+  return typeof o === "object" && !Array.isArray(o) && o !== null;
+}
+
+/**
+ * Diffs two objects, only return the keys that were modified
+ */
+function diff(a, b) {
+  const result = {};
+
+  Object.keys(a).forEach((keyA) => {
+    if (b[keyA]) {
+      if (!deepEqual(b[keyA], a[keyA])) {
+        if (isObject(b[keyA])) {
+          result[keyA] = diff(a[keyA], b[keyA]);
+        } else {
+          result[keyA] = b[keyA];
+        }
+      }
+    }
+  });
+
+  return result;
+}
+
+module.exports = diff;
