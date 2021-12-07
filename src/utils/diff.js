e42fa6b66214a27bb356e87c387283b6050d1abd
Joseph HENRY <josephenry@protonmail.com>
2021-12-07 10:55:21 +0100

    fix: diff with boolean values

--- src/utils/diff.js
+++ src/utils/diff.js
@@ -1,16 +1,15 @@
 /**
  * Taken from https://stackoverflow.com/questions/25456013/javascript-deepequal-comparison/25456134
  */
-function deepEqual(x, y) {
-  if (x === y) {
-    return true;
-  } else if (
-    typeof x == "object" &&
-    x != null &&
-    typeof y == "object" &&
+function deepEqual(x, y, debug = false) {
+  if (debug) console.log(x, y);
+  if (
+    typeof x === "object" &&
+    x !== null &&
+    typeof y === "object" &&
     y != null
   ) {
-    if (Object.keys(x).length != Object.keys(y).length) return false;
+    if (Object.keys(x).length !== Object.keys(y).length) return false;
 
     for (const prop in x) {
       if (Object.prototype.hasOwnProperty.call(y, prop)) {
@@ -18,6 +17,8 @@ function deepEqual(x, y) {
       } else return false;
     }
 
+    return true;
+  } else if (x === y) {
     return true;
   } else return false;
 }
@@ -33,7 +34,7 @@ function diff(a, b) {
   const result = {};
 
   Object.keys(a).forEach((keyA) => {
-    if (b[keyA]) {
+    if (typeof b[keyA] !== "undefined") {
       if (!deepEqual(b[keyA], a[keyA])) {
         if (isObject(b[keyA])) {
           result[keyA] = diff(a[keyA], b[keyA]);
